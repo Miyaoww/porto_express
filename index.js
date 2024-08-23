975d15d160c9b1339176e6985267b0738ffcfbd7
hooqii <hooqii.me@gmail.com>
2024-08-23 22:08:07 +0700

    update package.json

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ import express from "express";
 import routes from "./routes/routes.js";
 import path from "path";
 const app = express();
-const port = process.env.PORT ||3000
+const port = process.env.PORT || 3000;
 
 //setup static files
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 //ejs setup
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //create routes
-app.use('/', routes)
+app.use("/", routes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log error stack to the console
+  res.status(500).send("Something went wrong!"); // Send a generic error message
+});
 
-
-app.listen(port, ()=>{
-    console.log('server is running on port ${port}');
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
